a454ffeeb936f86e6c756515380818489d69537a
Batman @Moshi-W8 <aobphilos@gmail.com>
2016-06-05 23:51:38 +0700

    add user service & model

---
+++ api/services/UserService.js
@@ -0,0 +1,76 @@
+var _ = require('lodash');
+var Q = require('q');
+
+module.exports = {
+
+    findUser: function(u) {
+
+        var deferred = Q.defer();
+
+        // find user data
+        User.findOne({ userName: u.userName, password: u.password })
+            .exec(function(err, data) {
+
+                if (err) {
+                    deferred.reject(err);
+                } else {
+                    deferred.resolve(data);
+                }
+
+            });
+
+
+        return deferred.promise;
+    },
+
+    findOrCreateUser: function(u) {
+
+        var deferred = Q.defer();
+
+        // find user data
+        User.findOne({ userName: u.userName, password: u.password })
+            .exec(function(err, data) {
+
+                if (err) {
+                    deferred.reject(err);
+                } else {
+                    if (data) {
+                        deferred.resolve(data);
+                    } else {
+                        UserService.createUser(u)
+                            .then(function(user) {
+                                deferred.resolve(user);
+                            })
+                            .fail(function(userError) {
+                                deferred.reject(userError);
+                            });
+                    }
+                }
+
+            });
+
+
+        return deferred.promise;
+    },
+
+    createUser: function(u) {
+
+        var deferred = Q.defer();
+
+        // create user data
+        User.create(u)
+            .exec(function(err, data) {
+
+                if (err) {
+                    deferred.reject(err);
+                } else {
+                    deferred.resolve(data);
+                    console.log(data, ' had created.');
+                }
+
+            });
+
+        return deferred.promise;
+    }
+
+};
--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,13 +8,41 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
-
+var _ = require('lodash');
+var Q = require('q');
 
 module.exports.bootstrap = function(cb) {
 
     // It's very important to trigger this callback method when you are finished
     // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
 
+    // user constant
+    var userList = [
+        { userName: "admin", password: "panda" },
+        { userName: "siriraj", password: "password" },
+        { userName: "rajavithi", password: "password" },
+        { userName: "children", password: "password" },
+        { userName: "vajira", password: "password" },
+        { userName: "phramongkut", password: "password" },
+        { userName: "thammasat", password: "password" },
+        { userName: "msmchos", password: "password" },
+        { userName: "chulahos", password: "password" },
+        { userName: "songkla", password: "password" },
+        { userName: "ramathibodi", password: "password" },
+        { userName: "srinagarind", password: "password" },
+        { userName: "naresuan", password: "password" },
+        { userName: "burapha", password: "password" },
+    ]
 
-    cb();
+
+    var qInsert = _.map(userList, UserService.findOrCreateUser);
+
+    Q.all(qInsert)
+        .then(function() {
+
+            cb();
+        });
+
+
+
 };
